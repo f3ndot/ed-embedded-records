6b4866e8f9fd146f5f44aa4b8e75df6b1cdcc2ef
Taras Mankovski <taras@quandl.com>
2014-06-29 22:33:51 -0400

    Added dependent-relationships but it doesn't work

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
+import dependentRelationshipSetup from './utils/dependent-relationships';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
@@ -8,6 +9,8 @@ var App = Ember.Application.extend({
   modulePrefix: 'ed-embedded-records', // TODO: loaded via config
   Resolver: Resolver
 });
+
+dependentRelationshipSetup();
 
 /* global qd */
 qd.ajax.loadFixtures('ed-embedded-records/fixtures', '/');
--- app/models/invoice.js
+++ app/models/invoice.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   createDate: DS.attr('date'),
-  lines: DS.hasMany('line', {async: true}),
-  client: DS.belongsTo('client', {async: true})
+  lines: DS.hasMany('line', {async: true, dependent: true}),
+  client: DS.belongsTo('client', {async: true, dependent: true})
 });
